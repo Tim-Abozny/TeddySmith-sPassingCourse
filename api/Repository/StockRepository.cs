57b32f35eba0934004066212afef16ea7a7fec1c
bumblebee <tim.obozny@gmail.com>
2024-07-12 14:03:31 +0300

    Refactor to Repository

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.DTOs.Stock;
 using api.Interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -16,9 +17,57 @@ namespace api.Repository
         {
             _dbContext = context;
         }
-        public Task<List<Stock>> GetAllAsync()
+
+        public async Task<Stock> CreateAsync(Stock stockModel)
         {
-            return _dbContext.Stocks.ToListAsync();
+            await _dbContext.Stocks.AddAsync(stockModel);
+            await _dbContext.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stockModel = await _dbContext.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if(stockModel == null)
+            {
+                return null;
+            }
+
+            _dbContext.Stocks.Remove(stockModel);
+            await _dbContext.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<List<Stock>> GetAllAsync()
+        {
+            return await _dbContext.Stocks.ToListAsync();
+        }
+
+        public async Task<Stock?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Stocks.FindAsync(id);
+
+        }
+
+        public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
+        {
+            var existingStock = await _dbContext.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(existingStock == null)
+            {
+                return null;
+            }
+
+            existingStock.Symbol = stockDto.Symbol;
+            existingStock.CompanyName = stockDto.CompanyName;
+            existingStock.Purchase = stockDto.Purchase;
+            existingStock.LastDiv = stockDto.LastDiv;
+            existingStock.Industry = stockDto.Industry;
+            existingStock.MarketCap = stockDto.MarketCap;
+
+            await _dbContext.SaveChangesAsync();
+
+            return existingStock;
         }
     }
 }
